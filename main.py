2706bece8843a8b5af88eaf667ec6c7be99fc0a9
nikkxgod <n.fedyaev18@mail.ru>
2024-04-16 22:08:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ useragent = UserAgent()
 options = webdriver.ChromeOptions()
 options.add_argument(f"user-agent={useragent.random}")
 
-
 #stast
 driver = webdriver.Chrome(
     options=options
@@ -23,14 +22,3 @@ time.sleep(100)
 #     'http': 'http://wCxdCQ:gXaCbs@147.45.62.30:8000',
 # }
 
-# url = 'https://www.cloudbet32.com/ru/sports/tennis/today'  # URL сайта, к которому делаем запрос
-
-# try:
-#     response = requests.get(url, proxies=proxy)
-#     # Обработка ответа
-#     if response.status_code == 200:
-#         print(response.text)
-#     else:
-#         print("Ошибка при запросе:", response.status_code)
-# except requests.exceptions.RequestException as e:
-#     print("Ошибка при выполнении запроса:", e)
