1b05c21a9cd4e99e92f67b5375a45f0e5c87d8f6
nikkxgod <n.fedyaev18@mail.ru>
2024-04-21 12:29:41 +0300

    1
    1

--- project/main.py
+++ project/main.py
@@ -1,9 +1,20 @@
 import requests
-from bs4 import BeautifulSoup as BS
 
-r = requests.get("https://www.cloudbet32.com/ru/esports/counter-strike/international-t987e-cct-europe-qualifier/23189956/c1c46f5-ruby-v-ce75da-zero-tenacity")
-html = BS(r.content, 'html.parser')
+# Задаем параметры прокси и аутентификации
+proxy = {
+    'http': 'http://nMuoL2:HTX1m6@64.226.55.115:8000',
+    'https': 'http://nMuoL2:HTX1m6@64.226.55.115:8000'
+}
 
-result = html.find_all('data-component'=="market-tab")
-for i in result:
-    print(i.text)
+# Отправляем GET-запрос на сайт через прокси
+try:
+    response = requests.get('https://vncfgameinfo.365raylines.com/v2/odds?match_id=37944713', proxies=proxy)
+    # Проверяем успешность запроса
+    if response.status_code == 200:
+        print("Запрос успешно отправлен через прокси.")
+        print("Ответ от сервера:")
+        print(response.json())
+    else:
+        print(f"Произошла ошибка: {response.status_code}")
+except Exception as e:
+    print(f"Произошла ошибка при отправке запроса: {str(e)}")
